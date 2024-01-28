ee424b8e88d6166d2f544e124f53a98791049e33
nargesmn100 <95386498+nargesmn100@users.noreply.github.com>
2024-01-27 19:47:45 -0500

    Add files via upload

---
+++ 1-12 Multiplication Table.py
@@ -0,0 +1,47 @@
+#This program prints all the times tables from 1 to 12. 
+
+
+
+for number in range (1, 10):
+  answer_1 = 1 * number
+  answer_4 = 4 * number
+  answer_7 = 7 * number
+  answer_10 = 10 * number
+  print ('1 x', number, '=', answer_1, '    \t4 x', number, '=', answer_4, '    \t7 x', number, '=', answer_7, '     10 x', number, '=', answer_10)
+for number in range (10, 13):
+  answer_1 = 1 * number
+  answer_4 = 4 * number
+  answer_7 = 7 * number
+  answer_10 = 10 * number
+  print ('1 x', number, '=', answer_1, '    4 x', number, '=', answer_4, '    7 x', number, '=', answer_7, '    10 x', number, '=', answer_10)
+number += 1
+print()
+
+for number in range (1, 10):
+  answer_2 = 2 * number
+  answer_5 = 5 * number
+  answer_8 = 8 * number
+  answer_11 = 11 * number
+  print ('2 x', number, '=', answer_2, '    \t5 x', number, '=', answer_5, '    \t8 x', number, '=', answer_8, '     11 x', number, '=', answer_11)
+for number in range (10, 13):
+  answer_2 = 2 * number
+  answer_5 = 5 * number
+  answer_8 = 8 * number
+  answer_11 = 11 * number
+  print ('2 x', number, '=', answer_2, '    5 x', number, '=', answer_5, '    8 x', number, '=', answer_8, '    11 x', number, '=', answer_11)
+number += 1
+print()
+
+for number in range (1, 10):
+  answer_3 = 3 * number
+  answer_6 = 6 * number
+  answer_9 = 9 * number
+  answer_12 = 12 * number
+  print ('3 x', number, '=', answer_3, '    \t6 x', number, '=', answer_6, '    \t9 x', number, '=', answer_9, '     12 x', number, '=', answer_12)
+for number in range (10, 13):
+  answer_3 = 3 * number
+  answer_6 = 6 * number
+  answer_9 = 9 * number
+  answer_12 = 12 * number
+  print ('3 x', number, '=', answer_3, '    6 x', number, '=', answer_6, '    9 x', number, '=', answer_9, '    12 x', number, '=', answer_12)
+  number += 1
---
+++ Odd Numbers Range.py
@@ -0,0 +1,8 @@
+#This program displays all the odd numbers between 40 and 240.
+
+
+
+for number in range (40, 240):
+  if number % 2 != 0:
+    print (number, end= " ")
+print("\nTask completed successfully.")
